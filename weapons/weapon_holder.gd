238b1454b9282cdfb2fdb280860fca74ed80e425
ash4rk <cool.shark.98@mail.ru>
2023-05-25 09:04:48 +0300

    FIX: Weapon switch input

--- weapons/weapon_holder.gd
+++ weapons/weapon_holder.gd
@@ -6,26 +6,26 @@ enum WEAPONS {PISTOL, ASSAULT_RIFLE, SHOTGUN, BAZOOKA, SNIPER_RIFLE}
 
 var current_weapon = 1
 
+func _ready():
+	weapon_switch()
+
 func _input(_event):
 	# Using Match?
 	if Input.is_action_pressed("get_pistol"):
 		current_weapon = WEAPONS.PISTOL
+		weapon_switch()
 	if Input.is_action_pressed("get_assault_rifle"):
 		current_weapon = WEAPONS.ASSAULT_RIFLE
+		weapon_switch()
 	if Input.is_action_pressed("get_shotgun"):
 		current_weapon = WEAPONS.SHOTGUN
+		weapon_switch()
 	if Input.is_action_pressed("get_bazooka"):
 		current_weapon = WEAPONS.BAZOOKA
+		weapon_switch()
 	if Input.is_action_pressed("get_sniper_rifle"):
 		current_weapon = WEAPONS.SNIPER_RIFLE
-	weapon_switch()
-
-func _ready():
-	for child in get_child_count():
-		get_child(child).hide()
-		get_child(child).process_mode = Node.PROCESS_MODE_WHEN_PAUSED
-	get_child(current_weapon).show()
-	get_child(current_weapon).process_mode = Node.PROCESS_MODE_INHERIT
+		weapon_switch()
  
 func weapon_switch():
 	for child in get_child_count():
