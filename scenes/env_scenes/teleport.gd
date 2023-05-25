a4d2b669db221230fcd20b7a2d4a5cd3faadecfb
ash4rk <cool.shark.98@mail.ru>
2023-05-25 15:03:58 +0300

    FEAT: Introduce teleport

---
+++ scenes/env_scenes/teleport.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@export var exit_point: Node3D
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("players"):
+		body.global_position = exit_point.global_position
+		body.global_rotation = exit_point.global_rotation
