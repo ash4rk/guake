e3b5535b1e92ce0e9ac5f67f6c17d22949154b3c
ash4rk <cool.shark.98@mail.ru>
2023-05-26 13:53:35 +0300

    FEAT: Add options menu

--- addons/EasyMenus/Scripts/pause_menu_controller.gd
+++ addons/EasyMenus/Scripts/pause_menu_controller.gd
@@ -10,22 +10,22 @@ func open_pause_menu():
 	#Stops game and shows pause menu
 	get_tree().paused = true
 	show()
+	_toggle_mouse()
 	resume_game_button.grab_focus()
 	
 func close_pause_menu():
 	get_tree().paused = false
 	hide()
+	_toggle_mouse()
 	emit_signal("resume")
 
 func _on_resume_game_button_pressed():
 	close_pause_menu()
 
-
 func _on_options_button_pressed():
 	content.hide()
 	options_menu.show()
 	options_menu.on_open()
-
 
 func _on_options_menu_close():
 	options_menu.hide()
@@ -35,12 +35,21 @@ func _on_options_menu_close():
 func _on_quit_button_pressed():
 	get_tree().quit()
 
-
 func _on_back_to_menu_button_pressed():
 	close_pause_menu()
 	emit_signal("back_to_main_pressed")
+
+func _toggle_mouse():
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _input(event):
 	if (event.is_action_pressed("ui_cancel") or event.is_action_pressed("pause")) and visible and !options_menu.visible:
 		accept_event()
 		close_pause_menu()
+		return
+	if (event.is_action_pressed("ui_cancel") or event.is_action_pressed("pause")) and !visible:
+		accept_event()
+		open_pause_menu()
