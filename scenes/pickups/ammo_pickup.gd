25c11fbf030f6e2193eff02ac2f0d9a8ab09ab23
ash4rk <cool.shark.98@mail.ru>
2023-05-27 14:29:37 +0300

    FEAT: Update ammo pickup

--- scenes/pickups/ammo_pickup.gd
+++ scenes/pickups/ammo_pickup.gd
@@ -3,6 +3,14 @@ extends Pickup
 @export var AMMO_VALUE: int = 5
 @export var WEAPON_ID: int = WeaponHolder.WEAPONS.ASSAULT_RIFLE
 
+func _ready():
+	match WEAPON_ID:
+		WeaponHolder.WEAPONS.SHOTGUN:
+			$AmmoPickupMesh.material_override = $AmmoPickupMesh.material_override.duplicate()
+			$AmmoPickupMesh.material_override.albedo_color = Color.CORAL
+			$SpotLight3D.light_color = Color.CORAL
+
+			
 func on_pickup(player: Player):
 	player.gain_ammo(WEAPON_ID, AMMO_VALUE)
 	$AudioStreamPlayer3D.play()
